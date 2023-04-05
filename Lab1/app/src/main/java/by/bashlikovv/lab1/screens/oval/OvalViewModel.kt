e5ab9f71f7e1f85edfb5875decbb9b5f828af01c
BashlikovV <90571901+BashlikovV@users.noreply.github.com>
2023-04-05 09:53:17 +0300

    OvalViewModel JsonSerialization implemented.

--- Lab1/app/src/main/java/by/bashlikovv/lab1/screens/oval/OvalViewModel.kt
+++ Lab1/app/src/main/java/by/bashlikovv/lab1/screens/oval/OvalViewModel.kt
@@ -1,16 +1,76 @@
 package by.bashlikovv.lab1.screens.oval
 
+import android.content.Context
+import android.os.Parcel
+import android.os.Parcelable
 import androidx.compose.ui.geometry.Size
 import androidx.lifecycle.ViewModel
+import by.bashlikovv.lab1.screens.circle.CircleViewModel
+import by.bashlikovv.lab1.shapes.Drawing
 import by.bashlikovv.lab1.shapes.Oval
+import by.bashlikovv.lab1.utils.JsonSerialization
+import com.google.gson.Gson
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
 
-class OvalViewModel : ViewModel() {
+class OvalViewModel(
+    private val context: Context
+) : ViewModel(), JsonSerialization {
 
     private val _ovalUiState = MutableStateFlow(Oval())
     val ovalUiState = _ovalUiState.asStateFlow()
+
+    private val _drawingUiState = MutableStateFlow(Drawing())
+    val drawingUiState = _drawingUiState.asStateFlow()
+
+    override val gson = Gson()
+
+    override fun <T>String.parseJson(clazz: Class<T>): T {
+        return gson.fromJson(this, clazz)
+    }
+
+    override fun <T> T.parseToJson(): String {
+        return gson.toJson(this)
+    }
+
+    override fun getJsonFromFile() {
+        val file = File(context.filesDir, OVAL_FILE_NAME)
+        if (file.exists()) {
+            val fileInputStream = FileInputStream(file)
+            val str = fileInputStream.readAllBytes().decodeToString()
+            if (str.isNotEmpty()) {
+                try {
+                    val loadedState = str.parseJson(Oval::class.java)
+                    _ovalUiState.update { loadedState }
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+        }
+    }
+
+    override fun saveJsonToFile() {
+        val file = File(context.filesDir, OVAL_FILE_NAME)
+        if (!file.exists()) {
+            file.createNewFile()
+        }
+        val fileOutputStream = FileOutputStream(file)
+        val tmp = _ovalUiState.value
+        try {
+            val json = tmp.parseToJson()
+            fileOutputStream.write(json.toByteArray())
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    companion object {
+        const val OVAL_FILE_NAME = "serialization.oval"
+    }
 
     fun applyOval(oval: Oval) {
         _ovalUiState.update { oval }
