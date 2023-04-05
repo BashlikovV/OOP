3690fbf4bb1c82f4e01de05ebc6c26b51aac63fd
BashlikovV <90571901+BashlikovV@users.noreply.github.com>
2023-04-05 09:52:02 +0300

    Circle modified.

--- Lab1/app/src/main/java/by/bashlikovv/lab1/shapes/Circle.kt
+++ Lab1/app/src/main/java/by/bashlikovv/lab1/shapes/Circle.kt
@@ -2,6 +2,7 @@ package by.bashlikovv.lab1.shapes
 
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import java.io.Serializable
 
 open class Circle(
     x: Float = 0f,
@@ -9,15 +10,15 @@ open class Circle(
     color: Color = Color.White,
     translateL: Float = 0f,
     translateR: Float = 0f,
+    val center: Offset = Offset(x, y),
     val radius: Float = 0f,
-) : Square(x = x, y = y, color = color, translateL = translateL, translateR = translateR) {
+) : Square(x = x, y = y, color = color, translateL = translateL, translateR = translateR), Serializable {
+
+    constructor() : this(0f, 0f, Color.White, 0f, 0f, Offset(0f, 0f),0f)
 
     companion object {
         const val PI = 3.1415927f
     }
-
-    val center: Offset
-        get() = Offset(x, y)
 
     fun copy(
         x: Float = this.x,
@@ -26,8 +27,9 @@ open class Circle(
         translateL: Float = this.translateL,
         translateR: Float = this.translateR,
         radius: Float = this.radius,
+        center: Offset = this.center
     ): Circle {
-        return Circle(x, y, color, translateL, translateR, radius)
+        return Circle(x, y, color, translateL, translateR, center, radius)
     }
 
     /**
