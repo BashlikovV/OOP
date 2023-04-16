f1a897f6a7e2b3e092dfa4e1d0bc907994c78e58
BashlikovV <90571901+BashlikovV@users.noreply.github.com>
2023-04-16 22:52:50 +0300

    New screen added.

--- Lab1/app/src/main/java/by/bashlikovv/lab1/MainActivityContent.kt
+++ Lab1/app/src/main/java/by/bashlikovv/lab1/MainActivityContent.kt
@@ -1,7 +1,10 @@
 package by.bashlikovv.lab1
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material3.NavigationBar
@@ -10,12 +13,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import by.bashlikovv.lab1.screens.circle.CircleScreen
+import by.bashlikovv.lab1.screens.configuration.ConfigurationScreen
 import by.bashlikovv.lab1.screens.drawing.DrawingScreen
 import by.bashlikovv.lab1.screens.oval.OvalScreen
 import by.bashlikovv.lab1.screens.square.SquareScreen
@@ -24,9 +29,11 @@ enum class Screens {
     SQUARE,
     CIRCLE,
     OVAL,
-    DRAWING
+    DRAWING,
+    CONFIGURATION
 }
 
+@RequiresApi(Build.VERSION_CODES.TIRAMISU)
 @Composable
 fun MainActivityContent(
     modifier: Modifier = Modifier,
@@ -35,15 +42,20 @@ fun MainActivityContent(
     val navHostController = rememberNavController()
 
     val mainActivityUiState by mainActivityViewModel.mainActivityUiState.collectAsState()
+    val screenWidth = LocalConfiguration.current.screenWidthDp
 
-    NavigationBar(modifier = modifier.fillMaxWidth(), containerColor = MaterialTheme.colors.background) {
+    NavigationBar(
+        modifier = modifier.fillMaxWidth(),
+        containerColor = MaterialTheme.colors.background
+    ) {
         NavigationBarItem(
             selected = mainActivityUiState.selectedItem.name == Screens.SQUARE.name,
             onClick = {
                 mainActivityViewModel.onSelectItem(Screens.SQUARE)
                 navHostController.navigate(Screens.SQUARE.name)
             },
-            icon = { Text(text = "SQUARE", modifier = Modifier.padding(2.5.dp)) }
+            icon = { Text(text = "SQUARE", modifier = Modifier.padding(2.5.dp)) },
+            modifier = Modifier.width((screenWidth / 5).dp)
         )
         NavigationBarItem(
             selected = mainActivityUiState.selectedItem.name == Screens.CIRCLE.name,
@@ -52,7 +64,8 @@ fun MainActivityContent(
                 navHostController.navigate(Screens.CIRCLE.name)
 
             },
-            icon = { Text(text = "CIRCLE", modifier = Modifier.padding(2.5.dp)) }
+            icon = { Text(text = "CIRCLE", modifier = Modifier.padding(2.5.dp)) },
+            modifier = Modifier.width((screenWidth / 5).dp)
         )
         NavigationBarItem(
             selected = mainActivityUiState.selectedItem.name == Screens.OVAL.name,
@@ -60,7 +73,8 @@ fun MainActivityContent(
                 mainActivityViewModel.onSelectItem(Screens.OVAL)
                 navHostController.navigate(Screens.OVAL.name)
             },
-            icon = { Text(text = "OVAL", modifier = Modifier.padding(2.5.dp)) }
+            icon = { Text(text = "OVAL", modifier = Modifier.padding(2.5.dp)) },
+            modifier = Modifier.width((screenWidth / 5).dp)
         )
         NavigationBarItem(
             selected = mainActivityUiState.selectedItem.name == Screens.DRAWING.name,
@@ -68,7 +82,17 @@ fun MainActivityContent(
                 mainActivityViewModel.onSelectItem(Screens.DRAWING)
                 navHostController.navigate(Screens.DRAWING.name)
             },
-            icon = { Text(text = "DRAWING", modifier = Modifier.padding(2.5.dp)) }
+            icon = { Text(text = "DRAWING", modifier = Modifier.padding(2.5.dp)) },
+            modifier = Modifier.width((screenWidth / 5).dp)
+        )
+        NavigationBarItem(
+            selected = mainActivityUiState.selectedItem.name == Screens.CONFIGURATION.name,
+            onClick = {
+                mainActivityViewModel.onSelectItem(Screens.CONFIGURATION)
+                navHostController.navigate(Screens.CONFIGURATION.name)
+            },
+            icon = { Text(text = "CONFIGURATION", modifier = Modifier.padding(2.5.dp)) },
+            modifier = Modifier.width((screenWidth / 5).dp)
         )
     }
     NavHost(
@@ -87,6 +111,9 @@ fun MainActivityContent(
         }
         composable(Screens.DRAWING.name) {
             DrawingScreen(modifier = Modifier.fillMaxWidth())
+        }
+        composable(Screens.CONFIGURATION.name) {
+            ConfigurationScreen(modifier = Modifier.fillMaxWidth())
         }
     }
 }
