286adc91a14e2e923d9bbdd125b1cc790d31c8ca
BashlikovV <90571901+BashlikovV@users.noreply.github.com>
2023-04-19 10:01:28 +0300

    ConfigurationScreen added.

--- Lab1/app/src/main/java/by/bashlikovv/lab1/screens/configuration/Configuration.kt
+++ Lab1/app/src/main/java/by/bashlikovv/lab1/screens/configuration/Configuration.kt
@@ -1,24 +1,43 @@
 package by.bashlikovv.lab1.screens.configuration
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextOverflow
-import by.bashlikovv.lab1.shapes.Circle
-import by.bashlikovv.lab1.shapes.Drawing
-import by.bashlikovv.lab1.shapes.Oval
-import by.bashlikovv.lab1.shapes.Square
+import by.bashlikovv.lab1.screens.circle.CircleView
+import by.bashlikovv.lab1.screens.drawing.DrawingView
+import by.bashlikovv.lab1.screens.oval.OvalView
+import by.bashlikovv.lab1.screens.square.SquareView
+import java.io.File
+import java.net.URLClassLoader
 
+@RequiresApi(Build.VERSION_CODES.O)
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ConfigurationScreen(
     modifier: Modifier = Modifier
 ) {
     var state by remember { mutableStateOf(0) }
-    val titles = listOf("Circle", "Drawing", "Oval", "Square")
-    val className = Class.forName("by.bashlikovv.lab1.shapes.${titles[state]}")
+    val titles = listOf(
+        "circle" to "CircleView",
+        "drawing" to "DrawingView",
+        "oval" to "OvalView",
+        "square" to "SquareView"
+    )
+    val pluginFile = File(
+        "/home/bashlykovvv/Bsuir/OOP/Lab1/app/src/main/java/by/bashlikovv/lab1/screens.jar"
+    )
+    val className = try {
+        Class.forName("by.bashlikovv.lab1.screens.${titles[state].first}.${titles[state].second}")
+    } catch (e: Exception) {
+        val classLoader = URLClassLoader(arrayOf(pluginFile.toURI().toURL()))
+        classLoader.loadClass("screens.${titles[state].first}.${titles[state].second}")
+    }
 
     Scaffold(
         modifier = modifier,
@@ -29,7 +48,7 @@ fun ConfigurationScreen(
                         selected = state == index,
                         onClick = { state = index },
                         text = {
-                            Text(text = title, maxLines = 2, overflow = TextOverflow.Ellipsis)
+                            Text(text = title.first, maxLines = 2, overflow = TextOverflow.Ellipsis)
                         }
                     )
                 }
@@ -38,18 +57,25 @@ fun ConfigurationScreen(
     ) {
         Column(modifier = Modifier.padding(it)) {
             when (className) {
-                Circle::class.java -> {
-                    Text("Circle")
-                    val clazz = className.newInstance()
+                CircleView::class.java -> {
+                    className.newInstance().CircleScreen(
+                        modifier = Modifier.fillMaxSize()
+                    )
                 }
-                Drawing::class.java -> {
-                    Text("Draw")
+                DrawingView::class.java -> {
+                    className.newInstance().DrawingScreen(
+                        modifier = Modifier.fillMaxSize()
+                    )
                 }
-                Oval::class.java -> {
-                    Text("Oval")
+                OvalView::class.java -> {
+                    className.newInstance().OvalScreen(
+                        modifier = Modifier.fillMaxSize()
+                    )
                 }
-                Square::class.java -> {
-                    Text("Square")
+                SquareView::class.java -> {
+                    className.newInstance().SquareScreen(
+                        modifier = Modifier.fillMaxSize()
+                    )
                 }
             }
         }
